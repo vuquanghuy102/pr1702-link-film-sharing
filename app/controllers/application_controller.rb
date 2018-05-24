18d489ca7e8d61c9a0505f10f69f9d068b36269d
Huy <vuquanghuy001@gmail.com>
2018-05-24 07:18:42 +0700

    omniauth_facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  before_action :set_category
+
+  def set_category
+    @category=Category.all
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,4 +280,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  config.omniauth :facebook, "439113253201411", "bc28881b9b06060e593d6533a5ba044a"
 end
+
