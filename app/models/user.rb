8751d8af80e577a7a8fe8c3f6141230d83455086
NDK1410 <khanh.nd1410@gmail.com>
2018-06-13 20:51:59 +0700

    following-by-tut

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,19 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   ratyrate_rater
+
+  def friends
+    (following + followers).uniq
+  end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -27,5 +39,20 @@ class User < ApplicationRecord
         user.email = data["email"] if user.email.blank?
       end
     end
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
