610ecd146641e034454d109717c9d6f3254e0fc0
Huy <vuquanghuy001@gmail.com>
2018-06-15 19:41:29 +0700

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_search_params
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, alert: exception.message
+  end
+
   private
 
   def set_search_params
