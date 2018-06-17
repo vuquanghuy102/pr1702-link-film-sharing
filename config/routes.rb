02c61fdfea53306004ca25349d46c7d2912e891a
Huy <vuquanghuy001@gmail.com>
2018-06-17 21:49:30 +0700

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root "static_pages#home"
-  get "/news",    to: "static_pages#news"
-  get "/contact", to: "static_pages#contact"
-  get "/genres",  to: "static_pages#genres"
-  post "/rate",   to: "rater#create"
+  scope '(:locale)' do
+    root "static_pages#home"
+    get "/news",    to: "static_pages#news"
+    get "/contact", to: "static_pages#contact"
+    get "/genres",  to: "static_pages#genres"
+    post "/rate",   to: "rater#create"
 
-  resources :posts do
-    resources :comments
+    resources :posts do
+      resources :comments
+    end
+    resources :users, only: [:show, :index]
+    resources :search, only: [:index]
+    resources :categories, only: [:index, :show]
   end
-  resources :users, only: [:show, :index]
-  resources :search, only: [:index]
-  resources :categories, only: [:index, :show]
 end
